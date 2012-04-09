079e0a890dfade4e1a723fc13143fc45c1faae38
dahool <gabriel@opensuse.org>
2012-04-09 11:49:01 -0300

    change gettext to lazy in forms

--- files/forms.py
+++ files/forms.py
@@ -1,5 +1,5 @@
 import re
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from django import forms
 from django.conf import settings
 
--- news/forms.py
+++ news/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import ugettext as _
+from django.utils.translation import ugettext_lazy as _
 from news.models import *
 from django.conf import settings
 
