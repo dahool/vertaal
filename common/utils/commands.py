f9f683e4e2054d14cb146e684a09a33d85073cb2
Gabriel <gabriel@opensuse.org>
2012-05-24 10:13:49 -0300

    use subprocess instead of popen2

--- common/utils/commands.py
+++ common/utils/commands.py
@@ -1,14 +1,26 @@
-"""
-This code is extracted from transifex project (http://www.transifex.org/)
-Although I added some methods
+# -*- coding: utf-8 -*-
+"""Copyright (c) 2009-2012 Sergio Gabriel Teves
+All rights reserved.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 import os
-#import subprocess
-import popen2
 
-def get_command_output(cmd):
-    
+def get_command_output_popen(cmd):
+    import popen2
+
     if os.name == "posix":
         dout, din = popen2.popen2('{ ' + cmd + '; } 2>&1')
     else:
@@ -16,6 +28,27 @@ def get_command_output(cmd):
     res = dout.readlines()
     dout.close()
     din.close()
+    return res
+
+def get_command_output_subp(cmd):
+    import subprocess
+
+    if os.name == "posix":
+        command = '{ ' + cmd + '; } 2>&1'
+    else:
+        command = cmd + ' 2>&1'
+    proc = subprocess.Popen(command, shell=True, stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    dout, din = proc.stdout, proc.stdin
+    res = dout.readlines()
+    dout.close()
+    din.close()
+    return res
+
+def get_command_output(cmd):
+    try:
+        res = get_command_output_subp(cmd)
+    except Exception, e:
+        res = [str(e)]
     return res
 
 def python_to_args(**kwargs):
