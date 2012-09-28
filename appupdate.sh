78976a7a713628827bc7be55947d2f96a1f018c3
Gabriel <gabriel.sgt@gmail.com>
2012-09-28 15:15:06 -0300

    update script

--- appupdate.sh
+++ appupdate.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 echo "Applying update ... (this could take some time)"
-python -mcompileall .
+python -mcompileall . &>/dev/null
 python manage.py syncdb --noinput
 python manage.py appupdate
-python manage.py collectstatic --noinput
+#python manage.py collectstatic --noinput
+python manage.py evolve --execute --noinput
 python manage.py compilemessages
 echo "Reload"
-touch ../apache/django.wsgi
+touch wsgi.py
 echo ""
 echo "Finished"
 echo ""
