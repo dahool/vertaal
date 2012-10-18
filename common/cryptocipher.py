6cad917cebcb208a0f1eb5f239fe8b2fd509c0d3
Gabriel <gabriel.sgt@gmail.com>
2012-10-18 17:21:55 -0300

    remove pycrypto dependency

--- common/cryptocipher.py
+++ common/cryptocipher.py
@@ -1,4 +1,4 @@
-"""Copyright (c) 2009,2012 Sergio Gabriel Teves
+"""Copyright (c) 2009, Sergio Gabriel Teves
 All rights reserved.
 
 This program is free software: you can redistribute it and/or modify
@@ -18,46 +18,27 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from random import randrange
 import base64
-from blowfish import Blowfish
+from Crypto.Cipher import Blowfish
 from django.conf import settings
 
 class BCipher:
-    
-    prefix = 'CRYP='
-    
-    def __init__(self, key=None):
-        if not key:
-            key = getattr(settings, 'CIPHER_KEY', settings.SECRET_KEY)
-        if len(key) < 8: raise Exception('Key length must be greater than 8')
-        self.__cipher = Blowfish(key)
-        
+    def __init__(self, pwd=None):
+        if not pwd:
+            pwd = getattr(settings, 'SECRET_KEY')
+        self.__cipher = Blowfish.new(pwd)
     def encrypt(self, text):
-        padtext = self.__pad_text(text)
-        res = []
-        for n in range(0,len(padtext),8):
-            part = padtext[n:n+8]
-            res.append(self.__cipher.encrypt(part))
-        ciphertext = ''.join(res)
-        return self.prefix + base64.b64encode(ciphertext)
-    
+        ciphertext = self.__cipher.encrypt(self.__pad_file(text))
+        return base64.b64encode(ciphertext)
     def decrypt(self, b64text):
-        if not b64text.startswith(self.prefix):
-            return b64text
-        enctext = b64text[len(self.prefix):]
         try:
-            ciphertext = base64.b64decode(enctext)
+            ciphertext = base64.b64decode(b64text)
         except TypeError:
             # text is not encrypted
-            return enctext
-        res = []
-        for n in range(0,len(ciphertext),8):
-            part = ciphertext[n:n+8]
-            res.append(self.__cipher.decrypt(part))
-        cleartext = ''.join(res)
-        return self.__depad_text(cleartext)
-
+            return b64text
+        cleartext = self.__depad_file(self.__cipher.decrypt(ciphertext))
+        return cleartext
     # Blowfish cipher needs 8 byte blocks to work with
-    def __pad_text(self, text):
+    def __pad_file(self, text):
         pad_bytes = 8 - (len(text) % 8)
         # try to deal with unicode strings
         asc_text = str(text)
@@ -67,16 +48,14 @@ class BCipher:
         bflag = randrange(6, 248); bflag -= bflag % 8 - pad_bytes
         asc_text += chr(bflag)
         return asc_text
-
-    def __depad_text(self, text):
+    def __depad_file(self, text):
         pad_bytes = ord(text[-1]) % 8
         if not pad_bytes: pad_bytes = 8
         return text[:-pad_bytes]
-        
+
 if __name__ == '__main__':
-    import sys
     print "INIT TEST"
-    key = 'abcdefgh'
+    key = '%8%z7z3&*8*3t^h@j!!z953js5!3h^g%+1m9xcr17e!%dqb+2w'
     text = "este es un TEXTO que hay que encriptar"
     print "ENCRYPT: %s" % text
     bc = BCipher(key)
@@ -84,10 +63,6 @@ if __name__ == '__main__':
     print "RESULT: %s" % crypt
     res = bc.decrypt(crypt)
     print "DESCRYPTED: %s" % res
-    if len(sys.argv) > 1:
-        print "DECRYPTING INPUT: %s" % sys.argv[1]
-        res = bc.decrypt(sys.argv[1])
-        print "PLAIN TEXT: %s" % res
     if res == text:
         print "Success"
     else:
