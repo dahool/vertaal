37f53319a1eea095297377e084d2c8efc922795b
Gabriel <gabriel.sgt@gmail.com>
2013-02-12 18:57:32 -0300

    update wsgi script. minor fix in css

--- wsgi.py
+++ wsgi.py
@@ -5,5 +5,5 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "vertaal.settings")
 
-from django.core.handlers.wsgi import WSGIHandler
-application = WSGIHandler()
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
