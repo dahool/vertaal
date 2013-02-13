06a5c5a63ee2024dcaf2836e97a566389390f720
dahool <gabriel@opensuse.org>
2013-02-13 15:00:35 -0300

    disable compilation

--- appupdate.sh
+++ appupdate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Applying update ... (this could take some time)"
 sleep 2
-python -mcompileall . &>/dev/null
+#python -mcompileall . &>/dev/null
 python manage.py syncdb --noinput
 python manage.py appupdate
 #python manage.py collectstatic --noinput
