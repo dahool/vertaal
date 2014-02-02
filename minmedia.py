4ea050e21cd65880612ccacffdd2554bae0de35c
Gabriel <gabriel.sgt@gmail.com>
2014-02-02 12:54:33 -0300

    add js min option

---
+++ minmedia.py
@@ -0,0 +1,24 @@
+import os
+from os import listdir
+from os.path import isdir
+from common.utils.commands import get_command_output
+
+JS_FILES = ['base.js','djangopm.js','filedetail.js','filelist.js','jquery.alerts.src.js','jquery.fileupload.js','jquery.iframe-transport.js',
+'jquery.json.js','jquery.loading.js','jquery.measure.js','jquery.place.js','jquery.pulse.js','jquery.tablesorter.js','jquery.ui.draggable.js',
+'multiselectbox.js','orbited.js','profileapp.js','simpletooltip.js','simpletooltip2.js','stomp.js']
+
+CMD = 'java -jar yuicompressor-2.4.8.jar --nomunge --type %(type)s -o %(file)s --charset utf-8 %(file)s'
+
+def minimizejs(path):
+    for root, subf, files in os.walk(path):
+        for fs in files:
+            if fs in JS_FILES:
+                minfile(os.path.join(root, fs))
+
+def minfile(filename):
+    insize = os.path.getsize(filename)
+    print "Processing %s: %d ->" % (filename, insize),
+    cmd = CMD % {'type': 'js', 'file': filename}
+    get_command_output(cmd)
+    outsize = os.path.getsize(filename)
+    print "%d [%.2f%%]" % (outsize, ((insize-outsize)*100)/insize)
--- postupdate.py
+++ postupdate.py
@@ -8,6 +8,7 @@ import StringIO
 import settings
 import optparse
 import re
+from minmedia import minimizejs
 
 version = getattr(settings,'VERSION')
 target_path = getattr(settings,'CDN_PATH')
@@ -15,10 +16,12 @@ cdn_media_path = 'media/app/vertaal'
 source_path = getattr(settings,'STATIC_ROOT')
 
 def copy_media():
-    tgt = os.path.join(target_path, cnd_media_path, version)
+    tgt = os.path.join(target_path, cdn_media_path, version)
     if not os.path.exists(tgt):
         os.makedirs(tgt)
+    print "Prepare..."
     os.system("cp -R %s/* %s" % (source_path, tgt))
+    if prompt_min(): minimizejs(tgt)
     
 def upload():
     cmd = 'appcfg.py update %s' % target_path
@@ -30,7 +33,12 @@ def prompt_continue():
     s = "Update cdn version %s?" % version
     r = raw_input(s + " (Y/n): ")
     return not r or r == '' or r.lower() == 'y'
-    
+
+def prompt_min():
+    s = "Minimize JS?"
+    r = raw_input(s + " (Y/n): ")
+    return not r or r == '' or r.lower() == 'y'
+        
 def main():
     if prompt_continue():
         copy_media()
