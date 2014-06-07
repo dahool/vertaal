dc1c40e6160f771e4a23eac521362ea61aab6b23
Gabriel <gabriel.sgt@gmail.com>
2014-06-07 16:25:04 -0300

    fix password reset urls

--- registration/urls.py
+++ registration/urls.py
@@ -30,9 +30,14 @@ urlpatterns = patterns('',
     #url(r'^profile/$', account_profile, name="user_profile"),
     url(r'^signup/$', register, name="user_signup"),
     url(r'^query/$', query_user, name="user_query"),
-    url(r'^password_reset/$', auth_views.password_reset, name="password_reset"),
-    url(r'^password_reset/done/$', auth_views.password_reset_done),
-    url(r'^reset/(?P<uidb36>[0-9A-Za-z]+)-(?P<token>.+)/$', auth_views.password_reset_confirm),
-    url(r'^reset/done/$', auth_views.password_reset_complete),
     #url(r'^$', account_profile),
-)  
+    url(r'^password_change/$', auth_views.password_change, name='password_change'),
+    url(r'^password_change/done/$', auth_views.password_change_done, name='password_change_done'),
+    url(r'^password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^password_reset/done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$',
+        auth_views.password_reset_confirm, name='password_reset_confirm'),
+    url(r'^reset/done/$', auth_views.password_reset_complete, name='password_reset_complete'),        
+)
+    
+    
